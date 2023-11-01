60f8fdae1bcefb89e3f75fd1e3aeed70f446e5e9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-01 21:27:43 +0100

    feat: add UnoCSS preset forms

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,8 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno } from "unocss";
+import { presetForms } from "@julr/unocss-preset-forms";
 
 export default defineConfig({
   // ...UnoCSS options
-})
+  presets: [presetUno(), presetForms()],
+});
